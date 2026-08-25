ec21a80938a10ec8a9ba4fed365169e562414b8d
Monisha-star18 <monisha.r180505@gmail.com>
2026-08-25 15:22:30 +0530

    add comments for the redability

--- script/script.js
+++ script/script.js
@@ -2,57 +2,59 @@
 import {baseUrl} from './shared/shared.js' 
 import {Task} from './services/task.js'
 
+// the object of the task class 
 const taskObject =  new Task(baseUrl)
 
+//on click on the add task it calls the add task 
 $("#addTask-btn").on('click',function(){
     addTask()
 })  
 
+// on click on the clear it calls the clear function 
 $("#clear-btn").on('click',function(){
     clear()
 })
 
+    // function to create a new task and then post in the db
     async function addTask() 
     { 
         try
         {
+            //cardData function give the data object that have the task name , isdelected and date of creation 
             const CardDetails = cardData()
 
-              await taskObject.postTask(CardDetails)
+            // calls the function to post the task 
+            await taskObject.postTask(CardDetails)
 
+            // DISPLAY function is called so the post element can be showed 
             display()
-            delectedCardsDisplay()
-           
-
-                clear()
+            
+            //after the elemnt is posted the task name is been cleared 
+            clear()
 
         }
-        catch(err)
-        {
-            alert(err)
-        }
-        
-
+        catch(err) { alert(err) }
     }
 
+    // clear function used to clear the input form 
     function clear()
     {
         $("#todo-input").val('')
     }
 
-
+    // this function is used to create a object that is been posted when a task is created 
     function cardData()
     {
         let inputValue = $("#todo-input").val()
 
         if (inputValue.trim() != '')
         {
-            let taskDetails = {
-        taskName : inputValue ,
-        isDeleted : false,
-        dateOfCreation : new Date()}
+            // object that will be posted 
+            let taskDetails = { taskName : inputValue ,
+                                isDeleted : false,
+                                dateOfCreation : new Date() }
 
-        return taskDetails
+            return taskDetails
         }
         
         else
@@ -62,125 +64,164 @@ $("#clear-btn").on('click',function(){
         
     }
 
-    
-
-
-   
-
-    $(document).on('click','.delete-btn',async function(){
+    // when a delete button is clicked
+    $(document).on('click','.delete-btn',async function()
+    {
+        //the id of specific task is been stored 
         const deleteId = $(this).data('id')
+        
+        //function for softdelecte is called
         await taskObject.softdelect(deleteId)
+
+        //the functionality to refersh and show the currect cards and the show deleted cards 
         display()
         delectedCardsDisplay()
     })
 
-    
-
-
-    async function display() {
-        
-        try{
-             const displayTask = await taskObject.displayCards(false)
-
-              $(".display-container").empty()
-        displayTask.forEach(task => {
-           
-           const cardStruc = `<div class="col-5 card p-3 mx-2 my-2 bg-primary text-light">
-                                <div class="card-body">
-                                    <h6 class="card-title">${task.taskName}</h6>
-                                    <div class="btnGroup">
-                                        <button class="btn btn-warning edit-btn" data-id="${task.id}">Edit</button>
-                                        <button class="btn btn-danger delete-btn" data-id="${task.id}">Delete</button>
-                                    </div>
-                                </div>
-                            </div>`
-
-            $(".display-container").prepend (cardStruc)
-            
-        });
-        }
-        catch(err)
+    //function used to display all the task 
+    async function display() 
+    {
+        try
         {
-            alert(err)
+            // call the get function and store the values
+            const displayTask = await taskObject.displayCards(false)
+
+            $(".display-container").empty()
+            
+            //add the task in the task card and have a structure 
+            displayTask.forEach(task => 
+            {
+           
+                const cardStruc = `<div class="col-5 card p-3 mx-2 my-2 bg-primary text-light">
+                                        <div class="card-body">
+                                            <h6 class="card-title">${task.taskName}</h6>
+                                            <div class="btnGroup">
+                                                <button class="btn btn-warning edit-btn" data-id="${task.id}">Edit</button>
+                                                <button class="btn btn-danger delete-btn" data-id="${task.id}">Delete</button>
+                                            </div>
+                                        </div>
+                                    </div>`
+
+                $(".display-container").prepend (cardStruc)
+            
+            });
         }
+        catch(err) { alert(err) }
        
     }
 
-    async function delectedCardsDisplay() {
-        try{
-             const displayTask = await taskObject.displayCards(true)
-
-              $(".delected-task").empty()
-        displayTask.forEach(task => {
-           
-           const cardStruc = `<div class="col-5 card p-3 mx-2 my-2 bg-primary text-light">
-                                <div class="card-body">
-                                    <h6 class="card-title">${task.taskName}</h6>
-                                    <div class="btnGroup">
-                                        <button class="btn btn-light restore-btn" data-id="${task.id}">Restore</button>
-                                    </div>
-                                </div>
-                            </div>`
-
-            $(".delected-task").prepend (cardStruc)
-            
-        });
-        }
-        catch(err)
+    //function used to display all the task in delete
+    async function delectedCardsDisplay() 
+    {
+        try
         {
-            alert(err)
+            // call the get function and store the values of the deleted task
+            const displayTask = await taskObject.displayCards(true)
+
+            $(".delected-task").empty()
+
+            //add the task in the task card and have a structure 
+            displayTask.forEach(task => 
+            {
+           
+                const cardStruc = `<div class="col-5 card p-3 mx-2 my-2 bg-primary text-light">
+                                        <div class="card-body">
+                                            <h6 class="card-title">${task.taskName}</h6>
+                                            <div class="btnGroup">
+                                                <button class="btn btn-light restore-btn" data-id="${task.id}">Restore</button>
+                                            </div>
+                                        </div>
+                                    </div>`
+
+                $(".delected-task").prepend (cardStruc)
+            
+            });
         }
+        catch(err) { alert(err) }
     }
 
-    $(document).on('click','.restore-btn',async function(){
+    // when a restore button is clicked
+    $(document).on('click','.restore-btn',async function()
+    {
+        //the id of specific task is been stored 
         const restoreId = $(this).data('id')
+        
+        //function for restore is called
         await taskObject.restore(restoreId)
+
+        //the functionality to refersh and show the currect cards and the show deleted cards 
         display()
         delectedCardsDisplay()
     })
 
-
+    // made the edit id as global so can be used when needed 
     let editId
-     $(document).on('click','.edit-btn',function(){
-         editId = $(this).data('id')
+
+    // when a restore button is clicked
+    $(document).on('click','.edit-btn',function()
+    {
+        //the id of specific task is been stored 
+        editId = $(this).data('id')
+        //the edit function is called
         Edit(editId)
     })
 
-    $(document).on('click','.editModal-btn',async function(){
-        
-         const editedTaskName =  $("#edit-text").val()
+    // when a edit button on the modal is clicked button is clicked
+    $(document).on('click','.editModal-btn',async function()
+    {
+        // the task name edit is store 
+        const editedTaskName =  $("#edit-text").val()
 
-         const originalEdit = await Edit(editId)
+        //the original task name is stored 
+        const originalEdit = await Edit(editId)
 
-         if(originalEdit === editedTaskName)
+        // check whether anything is chnaged to get stored 
+        if(originalEdit === editedTaskName)
         {
             alert("edit the task name")
         }
+
         else
         {
-            const bodyContent = { taskName : editedTaskName}
+            // make the body object to post as its edited 
+            const bodyContent = { taskName : editedTaskName , updatedDate : new Date()}
+            
+            //post the data updated 
             await taskObject.edit(editId,bodyContent)
-             document.getElementById('editModal').style.display = 'none';
-             display()
+
+            //the edit modal is used 
+            document.getElementById('editModal').style.display = 'none';
+
+            //refersh and then task cards
+            display()
 
         }
     })
  
-    display()
-    delectedCardsDisplay() 
-async function Edit(editId )
+   
+    async function Edit(editId)
         {
-            try{
-                const edit = await taskObject.displayCardsid(false,editId)
-                const originalTaskName =  edit[0].taskName
-                $("#edit-text").val(originalTaskName)
-               document.getElementById('editModal').style.display = 'block';
-                return originalTaskName
-
-            }
-            catch(ERR)
+            try
             {
-                alert(ERR)
+                //get the data of the particular task card clicked 
+                const edit = await taskObject.displayCardsid(false,editId)
+
+                //the particular task name is extracted 
+                const originalTaskName =  edit[0].taskName
+
+                //the task name extracted is been filled in the edit modal 
+                $("#edit-text").val(originalTaskName)
+
+                //display the edit modal 
+                document.getElementById('editModal').style.display = 'block';
+
+                // return the task name do it can be checked in the time of posting
+                return originalTaskName
             }
+            catch(ERR) { alert(ERR) }
                 
         }
+
+    // display when its the not doing any functionality 
+    display()
+    delectedCardsDisplay() 
